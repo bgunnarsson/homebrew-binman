957d25b078b244674f09b5854485eecdc8a47fc9
bgunnarsson <dentadesign@gmail.com>
2026-03-24 22:34:13 +0000

    Fix build path:   cmd/binreq -> cmd/binman

--- binman.rb
+++ binman.rb
@@ -1,6 +1,6 @@
 class Binman < Formula
   desc "Terminal UI HTTP client for browsing and executing .http files"
-  homepage "https://github.com/bgunnarsson/binreq"
+  homepage "https://github.com/bgunnarsson/binman"
   url "https://github.com/bgunnarsson/binman/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "194748b1287d58b13e2064b4f7ac966b70937b96860d1649fdf015d8990c2f5d"
   license "MIT"
@@ -8,7 +8,7 @@ class Binman < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/"binman"), "./cmd/binreq"
+    system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/"binman"), "./cmd/binman"
   end
 
   test do

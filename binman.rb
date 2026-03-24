dd14b2817aeaa4aaac6a3640adc3f1c630796f8c
bgunnarsson <dentadesign@gmail.com>
2026-03-24 22:09:37 +0000

    update

--- binman.rb
+++ binman.rb
@@ -5,8 +5,6 @@ class Binman < Formula
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
-  bottle :unneeded
-
   depends_on "go" => :build
 
   def install

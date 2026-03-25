75b290753f38f7efd07a485fdc0b40a144fbf773
bgunnarsson <dentadesign@gmail.com>
2026-03-25 12:23:14 +0000

    binman 1.0.3

--- binman.rb
+++ binman.rb
@@ -1,8 +1,8 @@
 class Binman < Formula
   desc "Terminal UI HTTP client for browsing and executing .http files"
   homepage "https://github.com/bgunnarsson/binman"
-  url "https://github.com/bgunnarsson/binman/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "7dc1fa5332f913e3fb2af78296641a0e0aff1c71345692f8b237799b3bfd9f32"
+  url "https://github.com/bgunnarsson/binman/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "55af1e8257306d04af02e2b9e4f7d4b430525daf8bfd60fa223c95c557bc99b5"
   license "MIT"
 
   depends_on "go" => :build
